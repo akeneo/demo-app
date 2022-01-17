5da712cbc34d40a0d3489f93fa3d0405f1b6e606
Riff <riff.ventura@gmail.com>
2022-01-17 16:07:10 +0100

    CXP-987: Removed header comment

--- src/Controller/WelcomeAction.php
+++ src/Controller/WelcomeAction.php
@@ -7,10 +7,6 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @copyright 2022 Akeneo SAS (http://www.akeneo.com)
- * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
 class WelcomeAction
 {
     public function __construct(private \Twig\Environment $twig)
