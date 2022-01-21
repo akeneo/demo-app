fd0128f00dd86969ee141421ff5774ce071fb826
Guillaume Fer <guillaume.fer@akeneo.com>
2022-01-21 16:10:33 +0100

    CXP-1070-test: add the CallbackActionTest

---
+++ tests/Integration/Controller/CallbackActionTest.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\Controller;
+
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CallbackActionTest extends AbstractActionTest
+{
+    /**
+     * @test
+     */
+    public function itThrowsAExceptionWhenThePimUrlIsMissingInSession(): void
+    {
+        $client = $this->getClientWithSession([]);
+        $client->request('GET', '/authorization/activate');
+        $this->assertEquals(Response::HTTP_INTERNAL_SERVER_ERROR, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function itThrowsAExceptionWhenTheStateIsInvalid(): void
+    {
+        $client = $this->getClientWithSession([
+            'pim_url' => 'https://httpd/',
+            'state' => 'random_state_123456789',
+        ]);
+
+        $client->request('GET', '/callback?code=code&state=random_state_abcdefgh');
+
+        $this->assertEquals(Response::HTTP_INTERNAL_SERVER_ERROR, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function itThrowsAExceptionWhenTheCodeIsMissingInUrl(): void
+    {
+        $client = $this->getClientWithSession([
+            'pim_url' => 'https://example.com',
+            'state' => 'random_state_123456789',
+        ]);
+
+        $httpClient = $client->getContainer()->get(HttpClientInterface::class);
+        assert($httpClient instanceof MockHttpClient);
+
+        $httpClient->setResponseFactory([
+            new MockResponse(\json_encode(['access_token' => 'titi'])),
+        ]);
+        $client->request('GET', '/callback?state=random_state_123456789');
+
+        $this->assertEquals(Response::HTTP_INTERNAL_SERVER_ERROR, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function itThrowsAExceptionWhenAccessTokenIsMissing(): void
+    {
+        $client = $this->getClientWithSession([
+            'pim_url' => 'https://httpd/',
+            'state' => 'random_state_123456789',
+        ]);
+
+        $httpClient = $client->getContainer()->get(HttpClientInterface::class);
+        assert($httpClient instanceof MockHttpClient);
+
+        $httpClient->setResponseFactory([
+            new MockResponse(\json_encode([])),
+        ]);
+
+        $client->request('GET', '/callback?code=code&state=random_state_abcdefgh');
+
+        $this->assertEquals(Response::HTTP_INTERNAL_SERVER_ERROR, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function itRedirectsToTheAuthorizeUrlWithQueryParameters(): void
+    {
+        $client = $this->getClientWithSession([
+            'pim_url' => 'https://httpd/',
+            'state' => 'random_state_123456789',
+        ]);
+
+        $httpClient = $client->getContainer()->get(HttpClientInterface::class);
+        assert($httpClient instanceof MockHttpClient);
+
+        $httpClient->setResponseFactory([
+            new MockResponse(\json_encode(['access_token' => 'random_access_token'])),
+        ]);
+
+        $client->request('GET', '/callback?code=code&state=random_state_123456789');
+        $accessToken = $client->getRequest()->getSession()->get('akeneo_pim_access_token');
+        $this->assertEquals('random_access_token', $accessToken);
+        $this->assertResponseRedirects('/products', Response::HTTP_FOUND);
+    }
+}
