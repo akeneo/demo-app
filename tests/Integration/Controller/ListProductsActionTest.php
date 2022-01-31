108c35aa890b3964294e01f8d8c06e9830c26f51
willy mesnage <willy.mesnage@gmail.com>
2022-01-31 17:43:30 +0100

    CXP-979: test there is a link to the pim url

--- tests/Integration/Controller/ListProductsActionTest.php
+++ tests/Integration/Controller/ListProductsActionTest.php
@@ -49,20 +49,43 @@ class ListProductsActionTest extends AbstractActionTest
         $this->assertCount(10, $client->getCrawler()->filter('.product-card'));
     }
 
-    // TODO: uncomment this test after the merge of CXP-1082 : https://github.com/akeneo/demo-app/pull/12
-//    /**
-//     * @test
-//     */
-//    public function itRendersALinkThatTargetThePimUrl(): void
-//    {
-//        $pimUrl = 'https://httpd';
-//        $client = self::createClientWithSession([
-//            'pim_url' => $pimUrl,
-//            'akeneo_pim_access_token' => 'random_access_token_123456',
-//        ]);
-//
-//        $crawler = $client->request('GET', '/products');
-//
-//        $this->assertEquals($pimUrl, $crawler->selectLink('Go to Akeneo PIM')->attr('href'));
-//    }
+    /**
+     * @test
+     */
+    public function itRendersALinkThatTargetThePimUrl(): void
+    {
+        $pimUrl = 'https://httpd';
+        $client = self::createClientWithSession([
+            'pim_url' => $pimUrl,
+            'akeneo_pim_access_token' => 'random_access_token_123456',
+        ]);
+
+        $this->mockPimApiClientResponse(
+            $client,
+            new RequestMatcher('/api/rest/v1/locales', 'httpd', ['GET'], ['https']),
+            $this->getPimApiMockResponse('getLocalesListWithOnlyEnUs.json'),
+        );
+
+        $this->mockPimApiClientResponse(
+            $client,
+            new RequestMatcher('/api/rest/v1/products', 'httpd', ['GET'], ['https']),
+            $this->getPimApiMockResponse('getProductsListWith7TshirtsAnd4Caps.json'),
+        );
+
+        $this->mockPimApiClientResponse(
+            $client,
+            new RequestMatcher('/api/rest/v1/families/tshirt', 'httpd', ['GET'], ['https']),
+            $this->getPimApiMockResponse('getFamilyTshirt.json'),
+        );
+
+        $this->mockPimApiClientResponse(
+            $client,
+            new RequestMatcher('/api/rest/v1/families/cap', 'httpd', ['GET'], ['https']),
+            $this->getPimApiMockResponse('getFamilyCap.json'),
+        );
+
+        $client->request('GET', '/products');
+
+        $this->assertEquals($pimUrl, $client->getCrawler()->selectLink('Go to Akeneo PIM')->attr('href'));
+    }
 }
