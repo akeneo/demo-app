91fbcf21b2781a8ae9041ce1cde84d00f301845d
Tseho <quentin.favrie@gmail.com>
2022-02-05 10:29:12 +0100

    OCT-8: dirty workaround

--- src/Controller/ListProductsAction.php
+++ src/Controller/ListProductsAction.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Akeneo\Pim\ApiClient\Exception\HttpException;
 use App\Query\Locale\GuessCurrentLocaleQuery;
 use App\Query\Product\FetchProductsQuery;
+use App\Storage\AccessTokenStorageInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 use Twig\Environment as TwigEnvironment;
 
 final class ListProductsAction
@@ -17,18 +21,30 @@ final class ListProductsAction
         private TwigEnvironment $twig,
         private GuessCurrentLocaleQuery $guessCurrentLocaleQuery,
         private FetchProductsQuery $fetchProductsQuery,
+        private RouterInterface $router,
+        private AccessTokenStorageInterface $accessTokenStorage,
     ) {
     }
 
     #[Route('/products', name: 'products', methods: ['GET'])]
     public function __invoke(Request $request): Response
     {
-        $locale = ($this->guessCurrentLocaleQuery)();
+        try {
+            $locale = ($this->guessCurrentLocaleQuery)();
+            $products = ($this->fetchProductsQuery)($locale);
+        } catch (HttpException $e){
+            $this->accessTokenStorage->clear();
+            $session = $request->getSession();
+
+            return new RedirectResponse($this->router->generate('welcome', [
+                'pim_url' => $session->get('pim_url'),
+            ]));
+        }
 
         return new Response(
             $this->twig->render('products.html.twig', [
                 'locale' => $locale,
-                'products' => ($this->fetchProductsQuery)($locale),
+                'products' => $products,
             ])
         );
     }
