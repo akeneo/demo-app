2e9cc26483726e271abc2b599c9a4d1c29cde260
Tseho <quentin.favrie@gmail.com>
2022-02-22 15:07:29 +0100

    CXP-1069: enable NEG & circleci prod steps

---
+++ infra/terraform/application/network.tf
@@ -0,0 +1,10 @@
+resource "google_compute_region_network_endpoint_group" "default" {
+  project               = var.gcp_project_id
+  name                  = "default"
+  network_endpoint_type = "SERVERLESS"
+  region                = var.gcp_region
+
+  cloud_run {
+    service = google_cloud_run_service.app.name
+  }
+}
