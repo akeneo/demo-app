6c172c3c75782f7ee30f378858facfb4e501d549
Etienne Oliveau <etienne.oliveau@akeneo.com~>
2022-09-22 13:55:42 +0200

    CXP-1297 Debug tests

--- tests/Integration/Query/FetchProductsQueryTest.php
+++ tests/Integration/Query/FetchProductsQueryTest.php
@@ -53,30 +53,29 @@ class FetchProductsQueryTest extends AbstractIntegrationTest
     {
         $this->mockPimAPIResponse(
             'get-catalogs-store-us-products-empty-list.json',
-            'https://example.com/api/rest/v1/catalogs/catalog_store_us_id/products?limit=10',
+            'https://example.com/api/rest/v1/catalogs/catalog_store_us_products_empty_list/products?limit=10',
         );
 
         $query = static::getContainer()->get(FetchProductsQuery::class);
-        $result = $query?->fetch('en_US', 'catalog_store_us_id');
+        $result = $query?->fetch('en_US', 'catalog_store_us_products_empty_list');
 
         $expected = [];
 
         $this->assertEquals($expected, $result);
     }
 
-    /*
+    /**
      * @test
      */
-    /*
     public function itFetchesEmptyProducts(): void
     {
         $this->mockPimAPIResponse(
-            'get-products-empty.json',
-            'https://example.com/api/rest/v1/products?search=%7B%22identifier%22%3A%5B%7B%22operator%22%3A%22IN%22%2C%22value%22%3A%5B%22empty%22%5D%7D%5D%7D&locales=en_US&limit=10&with_count=false',
+            'get-catalogs-store-us-products-empty.json',
+            'https://example.com/api/rest/v1/catalogs/catalog_store_us_products_empty/products?limit=10',
         );
 
         $query = static::getContainer()->get(FetchProductsQuery::class);
-        $result = $query->fetch('en_US', ['empty']);
+        $result = $query->fetch('en_US', 'catalog_store_us_products_empty');
 
         $expected = [
             new Product('empty', '[empty]', []),
@@ -84,5 +83,4 @@ class FetchProductsQueryTest extends AbstractIntegrationTest
 
         $this->assertEquals($expected, $result);
     }
-    */
 }
